c90f461724eea12174953f823c17444a3af3ddf6
Denis Wagner <denis.wagner@valtech-mobility.com>
2021-10-20 16:06:08 +0200

    renames service and fixes build pipeline

--- commons/drogon-extended/lib/drogon/lib/inc/drogon/version.h
+++ commons/drogon-extended/lib/drogon/lib/inc/drogon/version.h
@@ -2,4 +2,4 @@
 #define MINOR 7
 #define PATCH 3
 #define DROGON_VERSION "1.7.3"
-#define DROGON_VERSION_SHA1 "33fbe2ac6f92f263cfdff12b6937e8d35dd7116c"
+#define DROGON_VERSION_SHA1 "7e1a6a1e6c4e07dbffe1aba801cfaae216f55493"
